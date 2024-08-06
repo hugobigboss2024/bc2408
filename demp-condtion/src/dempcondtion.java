25175af9f5c7477e947b957be31f080e7a5e2670
hugobigboss2024 <gavanboss2021@gmail.com>
2024-08-06 18:04:24 +0800

    6-8-2024

---
+++ demp-condtion/src/dempcondtion.java
@@ -0,0 +1,51 @@
+public class dempcondtion {
+public static void main(String[] args) {
+  int first = 31;
+  int secon = 42;
+ 
+
+
+  System.out.println("the first one" + first);
+  System.out.println("the second one" + secon);
+
+  int sum = first + secon;
+  System.out.println("the sum is" + sum);
+
+  int subtractio = first - secon;
+  System.out.println("the subtracion reuslt is" + subtractio);
+
+  byte b1 = 60;
+  byte b2 = 70;
+
+  sum = b1 + b2;
+
+  System.out.println("the another sum reuslt is " + sum );
+
+  boolean varB = true;
+  boolean varB2 = false;
+  System.out.println("the variable varB is" + varB);
+  System.out.println("the variable varC is" + varB2);
+
+  float fnumber = -130.20f;
+  double dnumber = -20.50f;
+
+  System.out.println("the variable is " + fnumber);
+  System.out.println("the variable is " + dnumber);
+
+  int a = 7;
+  int b = 3;
+  int reminder = a - b;
+  System.out.println("the variable reminder is " + reminder);
+
+  int e = 7;
+  int o = 8;
+  int y = 4;
+  int x = 10;
+
+  int result = e - o * x / y;
+  System.out.println("the result is" + result);
+
+    
+}
+  
+}
