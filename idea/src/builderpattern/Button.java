4902ac3445c2ea16dc4590d5019c241531d97ad6
hugobigboss2024 <gavanboss2021@gmail.com>
2024-09-15 21:19:06 +0800

    home

--- idea/src/builderpattern/Button.java
+++ idea/src/builderpattern/Button.java
@@ -5,10 +5,10 @@ public class Button {// 在 GUI 開發中，您可能需要組合多個屬性來
     private String color;
     private boolean isEnabled;
 
-    private Buttom(ButtonBuilder builder){
-        this.test = test;
-        this.color = color;
-        this.isEnabled = isEnabled;
+    private Button(ButtonBuilder builder) {
+        this.test = builder.test;
+        this.color = builder.color;
+        this.isEnabled = builder.isEnabled;
     }
 
     public static class ButtonBuilder {
@@ -16,5 +16,24 @@ public class Button {// 在 GUI 開發中，您可能需要組合多個屬性來
         private String color;
         private boolean isEnabled = true;
 
+        public ButtonBuilder(String test) {
+            this.test = test;
+        }
+
+        public ButtonBuilder color(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public ButtonBuilder inEnabled(boolean isEnabled) {
+            this.isEnabled = isEnabled;
+            return this;
+        }
+
+        public Button button() {
+            return new Button(this);
+        }
+
     }
+
 }
